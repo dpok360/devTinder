ea24f16dad898866f7adee2a5124d33abd8018b8
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2024-12-29 14:53:07 +0545

    explore routes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 
 //req handler
-app.use('/test', (req, res) => {
-  res.send('Hello form server');
+app.get('/user', (req, res) => {
+  res.send({ name: 'deepak', lastname: 'surya' });
+});
+app.post('/user', (req, res) => {
+  console.log('Save data to db');
+  res.send('Data successfully saved in db');
 });
 
 app.listen(3000, () => {
