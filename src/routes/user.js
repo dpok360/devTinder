f439ce7f15efbde4c526922571b02ea395f66093
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2025-01-03 13:46:31 +0545

    create feed route, fix user/connection

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { userAuth } = require('../utils/validation');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 const userRouter = express.Router();
 
@@ -28,11 +29,44 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
         { toUserId: loggedInUser._id, status: 'accepted' },
         { fromUserId: loggedInUser._id, status: 'accepted' },
       ],
-    }).populate('fromUserId', USER_SAFE_DATA);
+    })
+      .populate('fromUserId', USER_SAFE_DATA)
+      .populate('toUserId', USER_SAFE_DATA);
 
-    const data = connectionRequests.map((row) => row.fromUserId);
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+
+      return row.fromUserId;
+    });
 
     res.json({ data });
+  } catch (error) {
+    res.status(400).send({ ERROR: error.message });
+  }
+});
+
+userRouter.get('/feed', userAuth, async (req, res) => {
+  const USER_SAFE_DATA = 'firstName lastName photoUrl age gender about skills';
+  try {
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select('fromUserId , toUserId');
+
+    const hideUsersFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+    res.send({ users });
   } catch (error) {
     res.status(400).send({ ERROR: error.message });
   }
