ba892f80ea551f79bea85d62213b48f5c6a31458
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2025-01-03 11:29:18 +0545

    create user/connections route

--- src/routes/user.js
+++ src/routes/user.js
@@ -18,4 +18,24 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
   }
 });
 
+userRouter.get('/user/connections', userAuth, async (req, res) => {
+  const USER_SAFE_DATA = 'firstName lastName photoUrl age gender about skills';
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: 'accepted' },
+        { fromUserId: loggedInUser._id, status: 'accepted' },
+      ],
+    }).populate('fromUserId', USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => row.fromUserId);
+
+    res.json({ data });
+  } catch (error) {
+    res.status(400).send({ ERROR: error.message });
+  }
+});
+
 module.exports = userRouter;
