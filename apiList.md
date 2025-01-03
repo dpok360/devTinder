f84b615caba7babb08fb8bb3ab97d9689f6642ac
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2025-01-03 14:30:54 +0545

    add pagination in feed api

--- apiList.md
+++ apiList.md
@@ -19,8 +19,13 @@
 - POST /request/review/accepted/:requestId
 - POST /request/review/rejected/:userId
 
+## userRouter
+
 - GET /user/connections
 - GET /user/requests
 - GET /feed - gets you profile of others users
+
+- GET /feed?page=1&limit=10 =>first 10 users 1-10
+- GET /feed?page=2&limit=20 =>first 10 users 11-20
 
   Status: ignore, interested, accepted, rejected
--- src/routes/user.js
+++ src/routes/user.js
@@ -51,6 +51,12 @@ userRouter.get('/feed', userAuth, async (req, res) => {
   const USER_SAFE_DATA = 'firstName lastName photoUrl age gender about skills';
   try {
     const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
     const connectionRequest = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select('fromUserId , toUserId');
@@ -65,7 +71,10 @@ userRouter.get('/feed', userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
     res.send({ users });
   } catch (error) {
     res.status(400).send({ ERROR: error.message });
