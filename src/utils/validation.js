aa4e94b6969e68fee6d962c5f20bb067e02b1125
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2025-01-05 15:39:45 +0545

    fix userAuth tokken logic

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -17,7 +17,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error('Token is not valid');
+      return res.status(401).send('Please login');
     }
     const decodedobj = await jwt.verify(token, 'deepak@123');
     const { _id } = decodedobj;
